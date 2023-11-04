ffe2320e0d160ed3f79f2de5f89f964b76ab2b9f
Shanb98 <shanbasnayake@hotmail.com>
2023-11-04 20:01:30 +0530

    contact

--- controllers/contactContoller.js
+++ controllers/contactContoller.js
@@ -1,46 +1,86 @@
 const asyncHandler = require("express-async-handler");
-
+const  Contact = require("../models/contactModel")
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req,res) =>{
-    res.status(200).json({message: "Get all Contacts"});
-};
+const getContacts = asyncHandler(async (req, res) => {
+    //getting the contacts from the db
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  });
 
-//@desc Post all contacts
-//@route POST /api/contacts
+//@desc Post contact by id
+//@routs POST /api/contacts/
 //@access public
-const createContact = asyncHandler(async (req, res) =>{
-    console.log("The request body is :", req.body);
+const createContact = asyncHandler(async (req, res) => {
+    //printing the request body
+    console.log("The request body is : ", req.body);
+  
+    //destructuring the request body sent from the client side
     const { name, email, phone } = req.body;
+  
+    //validating the name,email and phone and displaying the error message
     if (!name || !email || !phone) {
       res.status(400);
-      throw new Error("All fields are mandatory !");
+      throw new Error("All fields are mandatory");
     }
-    res.status(201).json({message: "Create Contacts"});
-});
+    const contact = await Contact.create({
+      name, //equls to request.body.name. in es6 key and value is same we can use key
+      email,
+      phone
+    });
+    res.status(201).json(contact);
+  });
 
 //@desc Get new contacts
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req,res) =>{
-    res.status(200).json({message: `Get Contacts for ${req.params.id}`});
-};
+const getContact = asyncHandler(async (req, res) => {
+    //getting the contact by id
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
 
+    res.status(200).json(contact);
+  });
 //@desc Update all contacts
 //@route UPDATE /api/contacts/:id
 //@access public
-const updateContact = (req,res) =>{
-    res.status(200).json({message: `Update Contacts for ${req.params.id}`});
-};
+const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+  
+    const updatedContact = await Contact.findByIdAndUpdate(
+      req.params.id, //getting the id of the contact that needs to be updated
+      req.body, //getting the updated body
+      { new: true } //query option
+    );
+  
+  
+    res.status(200).json(updatedContact);
+  });
 
 //@desc Delete all contacts
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = (req,res) =>{
-    res.status(200).json({message: `Delete Contacts for ${req.params.id}`});
-};
-
+//@desc Delete a contact
+//@routs DELETE /api/contacts/:id
+//@access public
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+  
+    await Contact.deleteOne({ _id: req.params.id });
+    res.status(200).json(contact); 
+  });
 
 module.exports = {
     getContacts,
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const detenv = require("dotenv").config;
+const connectiondb = require("./config/dbConnection");
+const dotenv = require("dotenv"); // Import the 'dotenv' library
 const errorHandler = require("./middleware/errorHandler");
 
+// Call the 'config' method to load environment variables from the '.env' file
+dotenv.config();
+
+connectiondb();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
 app.use(errorHandler);
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
